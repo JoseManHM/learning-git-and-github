e5c418f3517d6c9af3fd66efa9ad1cc7defb8ba8
yo <dominio@jaja.com>
2020-06-07 09:02:51 -0500

    Hola, primer snapshot Primer commit

---
+++ Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+
+	/**
+	 * Programa que muestra el uso de variables
+	 */
+	public static void main(String[] args) {
+		byte edad;
+		edad = 20;
+		System.out.println(edad);
+		edad = 21;
+		System.out.println(edad);
+		edad = 22;
+		System.out.println(edad);
+		/*En las lineas anteriores podemos ver como el valor de la variable
+		cambia a lo largo del programa*/
+	}
+}
---
+++ main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+	/**
+	 * programa que muestra un mensaje en la consola
+	 */
+	public static void main(String[] args){
+		System.out.println("¡Hello world!");
+
+	}
+}
