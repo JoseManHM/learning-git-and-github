366a0adde1da184ac59300c789edca3599aabbb2
yo <dominio@jaja.com>
2020-06-07 23:52:27 -0500

    Se agregaron dos archivos java, que muestran el uso de constantes y operadores

---
+++ Constantes.java
@@ -0,0 +1,15 @@
+
+public class Constantes {
+
+	/**
+	 * Programa que muestra el uso de una constante
+	 * Programa que convierte de pulgadas a centimetros
+	 */
+	public static void main(String[] args) {
+		final double pulgada = 2.54; //Declaración de una constante
+		double cm = 6;
+		double resultado = cm/pulgada;
+		System.out.println(cm + " cm son: "+resultado+" pulgadas");
+	}
+
+}
---
+++ OperadoresConstantes.java
@@ -0,0 +1,18 @@
+
+public class OperadoresConstantes {
+
+	/**
+	 * Programa que muestra cómo se usan los constantes y los operadores.
+	 */
+	public static void main(String[] args) {
+		int a = 5;
+		int b;
+		b = 7;
+		int c = b + a; //Se utiliza por primera vez un operador.
+		c++; //se hace un uso de un operador de incremento
+		// si ponemos c+=10 incrementa el valor en 10 unidades
+		System.out.println("El valor de c es:"+c);//se hace uso de un concatenador
+		
+	}
+
+}
